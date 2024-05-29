85eb58e2b54b0743a55a24d780bd06c5b2f65a5a
First Fractal <firstfractal512@gmail.com>
2024-05-29 18:26:29 -0500

    made a function to do the cooldown for it

--- RainRandomEnemies.cs
+++ RainRandomEnemies.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace RainRandomEnemies
 {
@@ -11,4 +13,21 @@ namespace RainRandomEnemies
 	{
 
 	}
+
+	public class RainRandomEnemiesSystem : ModSystem
+	{
+        int cooldown = 0;
+        int cooldownMax = ffFunc.TimeToTick(5);
+
+        void test()
+        {
+            ffFunc.Talk("I've just came online", Color.White);
+        }
+
+        public override void PostUpdateEverything()
+        {
+            ffFunc.CooldownSystem(ref cooldown, ref cooldownMax, test);
+            base.PostUpdateEverything();
+        }
+    }
 }
