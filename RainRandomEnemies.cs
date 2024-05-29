cd162a5cc8b624b6108f2f52ce05e94d55779592
First Fractal <firstfractal512@gmail.com>
2024-05-29 17:46:13 -0500

    Add project files.

---
+++ RainRandomEnemies.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace RainRandomEnemies
+{
+	public class RainRandomEnemies : Mod
+	{
+
+	}
+}
